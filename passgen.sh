ab1e01a3bc1c32ef4142ffe2d78fa07614784309
Farzan Afringan <afringane@gmail.com>
2025-07-18 22:39:04 +0330

    Initial commit: add basic password generator script

---
+++ passgen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Simple password generator
+LENGTH=16
+
+# Generate password using /dev/urandom
+PASSWORD=$(tr -dc 'A-Za-z0-9!@#$%&*_' < /dev/urandom | head -c $LENGTH)
+
+echo "Generated password: $PASSWORD"
